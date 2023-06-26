a879e04bb0f2e906039517c952184e4f3f1f6594
ErtKid <avrit.max@gmail.com>
2023-06-26 11:23:18 +0200

    add models

---
+++ lib/models/Post.dart
@@ -0,0 +1,16 @@
+class Post {
+  final String id;
+  final String userId;
+  final String content;
+  final DateTime dateCreated;
+  final List<String>
+      likes; // Liste des IDs des utilisateurs qui ont aimé ce post.
+
+  Post({
+    required this.id,
+    required this.userId,
+    required this.content,
+    required this.dateCreated,
+    required this.likes,
+  });
+}
