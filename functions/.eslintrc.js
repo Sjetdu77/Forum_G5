ef5652886c0d3712454eb5e569a3cc3df8f46fd2
samiHEL <sami.hella.sio@gmail.com>
2023-06-27 14:10:41 +0200

    modification des problemes d'indentation avec eslint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -11,9 +11,9 @@ module.exports = {
     "google",
   ],
   rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "no-restricted-globals": "off",
+    "prefer-arrow-callback": "off",
+    "quotes": "off",
   },
   overrides: [
     {
